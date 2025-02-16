680079bb8a792dad91b2940d64c3ec9161722b20
yassvr <134147746+yassvr@users.noreply.github.com>
2025-02-16 18:37:32 +0100

    Update index.js with basic GraphQL server setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-// Initial setup for GraphQL practice
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// Basic GraphQL schema
+const schema = buildSchema(`
+  type Query {
+    message: String
+  }
+`);
+
+// Resolver for the query
+const root = {
+  message: () => 'Hello World!'
+};
+
+// Create express server
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
+app.listen(4000, () => {
+  console.log('Server running at http://localhost:4000/graphql');
+});
